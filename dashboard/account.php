4c5e6ade49e4f02ac666fa9e826b3852650805ae
Merlin <contact@merlin.ac>
2023-10-13 17:54:39 +0200

    Adding success message to delete file

--- dashboard/account.php
+++ dashboard/account.php
@@ -62,6 +62,22 @@ require("navbar.php");
             <div class="p-4 mb-4 bg-white rounded-lg shadow sm:p-6 xl:p-8 dark:bg-gray-800">
                 <div class="flow-root">
                     <h3 class="text-xl font-bold dark:text-white">Data</h3>
+                    <?php
+
+if (isset($_GET['delete'])) {
+    if ($_GET['delete'] == 'success') {
+
+?>
+        <div class="flex items-center w-full sm:w-11/12 p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>
+                <span class="font-medium">Success!</span> The file has been deleted.
+            </div>
+        </div>
+<?php } } ?>
                     <ul class="divide-y divide-gray-200 dark:divide-gray-700">
 <?php
 
